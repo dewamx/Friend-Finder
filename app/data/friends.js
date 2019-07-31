d8636ef112691fc7503337a8cf244f5ab66cfc5c
raxemmohamed <raxemmohamed@gmail.com>
2019-07-31 13:46:09 -0500

    make it better

---
+++ app/data/friends.js
@@ -0,0 +1,45 @@
+var friends = [
+    {
+        name: "Superman",
+        photo:
+            "https://static.comicvine.com/uploads/original/11114/111145484/4194724-9114309978-Super.jpg",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    },
+    {
+        name: "Batman",
+        photo:
+            "https://i.imgflip.com/7vwc2.jpg",
+        scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+    },
+    {
+        name: "Batman Beyond",
+        photo:
+            "https://cdn.vox-cdn.com/thumbor/DGGslg60Tg5Z-zAm96-XR8xX5po=/0x0:1200x676/1200x800/filters:focal(568x248:760x440)/cdn.vox-cdn.com/uploads/chorus_image/image/64759564/batman_beyond_139636_1_1200x676.0.jpg",
+        scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+    },
+    {
+        name: "Spiderman",
+        photo:
+            "https://i.chzbgr.com/full/3731957760/hDA29C22D/",
+        scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+    },
+    {
+        name: "Borat",
+        photo:
+            "https://upload.wikimedia.org/wikipedia/commons/7/7e/Borat_in_Cologne.jpg",
+        scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+    },
+    {
+        name: "Mario",
+        photo:
+            "https://upload.wikimedia.org/wikipedia/en/thumb/a/a9/MarioNSMBUDeluxe.png/220px-MarioNSMBUDeluxe.png",
+        scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
+    },
+    {
+        name: "Luigi",
+        photo:
+            "https://upload.wikimedia.org/wikipedia/en/thumb/f/f1/LuigiNSMBW.png/220px-LuigiNSMBW.png",
+        scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+    }
+];
+module.exports = friends;
